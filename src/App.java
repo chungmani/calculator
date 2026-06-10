c36750df9c0e8889f97b5d04a6daaa03a2389e0d
chungmani <cndaks19@naver.com>
2026-06-10 15:49:35 +0900

    Calculator 생성, 연산부분 일부 수정

--- src/App.java
+++ src/App.java
@@ -4,10 +4,10 @@ public class App {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        Calculator calculator = new Calculator();
 
-        String exit = "no"; // 반복문을 위해 변수 선언
         // 계산 후 exit 입력되면 반복 종료
-        while (!exit.equals("exit")) {
+        while (true) {
 
             // 숫자 초기화
             int num1 = 0;
@@ -24,37 +24,13 @@ public class App {
             sc.nextLine();
 
             // 계산하기
-            switch (cal) {
-                case "+":
-                    int plus = num1 + num2;
-                    System.out.println("결과: " + plus);
-                    break;
+            calculator.calculate(num1, cal, num2);
 
-                case "-":
-                    int minus = num1 - num2;
-                    System.out.println("결과: " + minus);
-                    break;
-
-                case "*":
-                    int multiply = num1 * num2;
-                    System.out.println("결과: " + multiply);
-                    break;
-
-                case "/":
-                    if (num2 != 0) {
-                        int devide = num1 / num2;
-                        System.out.println("결과: " + devide);
-                        break;
-                    } else {
-                        System.out.println("나눗셈에서는 분모가 0이 올 수 없습니다.");
-                        break;
-                    }
-
-                default:
-                    System.out.println("정확한 사칙연산 기호를 입력하세요 (+, - , * , /)");
-            }
             System.out.print("계속하시겠습니까? (exit 입력시 종료): ");
-            exit = sc.nextLine();
+            String exit = sc.next();
+            if (exit.equals("exit")) {
+                break;
+            }
         }
 
     }
---
+++ src/Calculator.java
@@ -0,0 +1,37 @@
+public class Calculator {
+
+    public int calculate(int num1, String cal, int num2) {
+        int result = 0;
+
+        switch (cal) {
+            case "+":
+                int add = num1 + num2;
+                System.out.println("결과: " + add);
+                return result = add;
+
+            case "-":
+                int sub = num1 - num2;
+                System.out.println("결과: " + sub);
+                return result = sub;
+
+            case "*":
+                int multiply = num1 * num2;
+                System.out.println("결과: " + multiply);
+                return result = multiply;
+
+            case "/":
+                if (num2 != 0) {
+                    int divide = num1 / num2;
+                    System.out.println("결과: " + divide);
+                    return result = divide;
+                } else {
+                    System.out.println("나눗셈에서는 분모가 0이 올 수 없습니다.");
+                    break;
+                }
+
+            default:
+                System.out.println("올바른 사칙연산을 기입하세요.");
+        }
+        return result;
+    }
+}
